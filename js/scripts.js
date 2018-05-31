bc229459c3fdf1b0e05f788cd3fe911f13e34ba3
Abel Trotter and Rita Bennett-Chew <abel@>
2018-05-31 14:20:12 -0700

    Add hide and show methods, add validation to enable/disable submit btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
-// create branching structure based on input here
+const ANIMAL_LIST = ["snake", "turtle", "insect"];
+
+function disableBtn() {
+  $("#submit").prop("disabled", "true");
+}
+
+function checkValue(input) {
+  for(i = 0; i < ANIMAL_LIST.length; i++) {
+    if (input === ANIMAL_LIST[i]) {
+      $("#submit").removeAttr("disabled");
+      break;
+    } else {
+      disableBtn();
+    }
+  }
+}
+
+function hideAnimals() {
+  $("#turtle").hide();
+  $("#insect").hide();
+  $("#snake").hide();
+}
+
 function checkAndDisplay(animalInput) {
   if (animalInput === "turtle") {
+    hideAnimals();
     $("#turtle").show();
   } else if (animalInput === "insect") {
+    hideAnimals();
     $("#insect").show();
   } else if (animalInput === "snake") {
+    hideAnimals();
     $("#snake").show();
   } else {
     console.log(animalInput);
@@ -12,6 +37,13 @@ function checkAndDisplay(animalInput) {
 }
 
 $(document).ready(function() {
+  disableBtn();
+
+  $("#animal").keyup(function() {
+    var input = $("#animal").val();
+    checkValue(input);
+  });
+
   $("#submit").click(function(e) {
     e.preventDefault();
     var input = $("#animal").val();
